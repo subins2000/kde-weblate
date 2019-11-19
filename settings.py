f60d8ae84f6ad2bd494bcafa0538195ecb5d640c
Subin Siby <mail@subinsb.com>
2019-11-19 06:38:57 +0530

    Add smc kde host

--- settings.py
+++ settings.py
@@ -743,7 +743,7 @@ SERVER_EMAIL = 'noreply@example.com'
 DEFAULT_FROM_EMAIL = 'noreply@example.com'
 
 # List of URLs your site is supposed to serve
-ALLOWED_HOSTS = ['kde-ml.anishsheela.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['kde-ml.anishsheela.com', 'kde.smc.org.in', 'localhost', '127.0.0.1']
 
 # Example configuration for caching
 # CACHES = {
